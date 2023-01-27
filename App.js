5b1f18119900896aff31bd5ddd987c8ccd8bfa95
Shanya Connell <shanyaconnell@gmail.com>
2023-01-27 15:21:10 -0500

    feat: created home screen

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+
 
 //1. Set up redux
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Uber Clone</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image 
+            style={{width: 100, height: 100, resizeMode: 'contain'}}
+            source={require('../assets/Reggae_Tours_White.jpg')}/>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'blue',
+    }
+});
