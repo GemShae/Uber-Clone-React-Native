3213c7584e774a950f04f448fa55d5df543709ca
Shanya Connell <shanyaconnell@gmail.com>
2023-01-31 12:03:41 -0500

    feat: updated origin navigation functionality

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,8 +3,11 @@ import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
 import MapView from 'react-native-maps';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const MapScreen = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
     <View>
       <View style={tw`h-1/2`}>
@@ -12,7 +15,9 @@ const MapScreen = () => {
       </View>
 
       <View style={tw`h-1/2`}>
-
+        <Stack.Navigator>
+          
+        </Stack.Navigator>
       </View>
     </View>
   )
